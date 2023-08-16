4c36f1ebbe27580565822beb7cf3ce3e20b3a5a6
Soma <soma.nutech@gmail.com>
2023-08-17 00:07:41 +0700

    push clear rumus

--- js/script.js
+++ js/script.js
@@ -24,6 +24,34 @@ function hitungLuas() {
     var detailRumus = document.getElementById("detailRumus");
     detailRumus.append('');
 
+  }
 
+  function hitungKeliling() {
+    const a = document.getElementById("sisi-a").value;
+    const b = document.getElementById("sisi-b").value;
+    const c = document.getElementById("sisi-c").value;
+
+    var detailRumusKeliling = document.getElementById("detailRumusKeliling");
+    
+    var detail = '';
+
+    const keliling = (parseInt(a) + parseInt(b) + parseInt(c));
+
+    detail += '<p>L = S1 x S2 x S3</p>';
+    detail += '<p>L = '+a+' + '+b+' + '+c+'</p>';
+    detail += '<p>L = '+keliling+'</p>';
+    
+    detailRumusKeliling.append(detail);
+  
+
+  }
+
+  function resetKeliling() {
+    document.getElementById("sisi-a").value = '';
+    document.getElementById("sisi-b").value = '';
+    document.getElementById("sisi-c").value = '';
+
+    var detailRumus = document.getElementById("detailRumusKeliling");
+    detailRumus.reset();
 
   }
